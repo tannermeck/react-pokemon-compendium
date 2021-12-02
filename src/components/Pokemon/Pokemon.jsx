a8dfbfb5471fbfd7eac7857e0059fa44ee970782
TANNER MECK <steve.meck246@gmail.com>
2021-12-02 10:37:16 -0800

    done with styling on individual pokemon

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -8,7 +8,9 @@ function Pokemon({ pokemon }){
                 <div key={character._id} className="pokeContainer">
                     <li className="pokemonName">{character.pokemon}</li>
                     <img className="pokemonImage" alt={character.pokemon} src={character.url_image} />
-                    <p className="pokemonType">{character.type_2 !== 'NA' ? `${character.type_1}/${character.type_2}` : `${character.type_1}`}</p>
+                    <p className="pokemonType">{character.type_2 !== 'NA' ? `Type: ${character.type_1}/${character.type_2}` : `Type: ${character.type_1}`}</p>
+                    <p className="pokemonType">{character.ability_2 !== 'NA' ? `Ability: ${character.ability_1}/${character.ability_2}` : `Ability: ${character.ability_1}`}</p>
+                    <p className="pokemonType">Defense Level: {character.defense}</p>
                 </div>
             ))}
         </ul>
