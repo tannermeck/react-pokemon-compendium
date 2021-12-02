4d3b7ba178640e7a0c0700159eb142e7e7464b9c
TANNER MECK <steve.meck246@gmail.com>
2021-12-01 17:35:40 -0800

    pokeball loader added

--- src/views/Compendium.jsx
+++ src/views/Compendium.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
+import './compendium.css';
+
 function Compendium(){
+    const [loading, setLoading] = useState(false)
+    // if (loading)
     return (
         <>
-            <h1>HI FROM Compendium</h1>
+            {loading ? 
+                <img id="pokeball" alt="pokeball" src='https://cdn.dribbble.com/users/621155/screenshots/2835314/simple_pokeball.gif'/> 
+                : <h1>Not Loading...</h1>   
+            }
         </>
     )
 }
