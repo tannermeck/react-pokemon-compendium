c58843e6686483cc47a75436443edfeea98018f3
TANNER MECK <steve.meck246@gmail.com>
2021-12-02 10:26:29 -0800

    character type_2 not displayed if equal to na

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -5,9 +5,10 @@ function Pokemon({ pokemon }){
     return (
         <ul>
             {pokemon.map(character => (
-                <div className="pokeContainer">
-                    <li className="pokemonName" key={character.pokemon}>{character.pokemon}</li>
+                <div key={character._id} className="pokeContainer">
+                    <li className="pokemonName">{character.pokemon}</li>
                     <img className="pokemonImage" alt={character.pokemon} src={character.url_image} />
+                    <p className="pokemonType">{character.type_2 !== 'NA' ? `${character.type_1}/${character.type_2}` : `${character.type_1}`}</p>
                 </div>
             ))}
         </ul>
